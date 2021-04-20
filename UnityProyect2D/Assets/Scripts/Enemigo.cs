479643d302de6e969920b506bf7d622acb8db2ea
malik korrich <61884452+T0xic-lab@users.noreply.github.com>
2021-04-20 17:03:39 +0200

    new push estable

--- UnityProyect2D/Assets/Scripts/Enemigo.cs
+++ UnityProyect2D/Assets/Scripts/Enemigo.cs
@@ -77,10 +77,10 @@ public class Enemigo : MonoBehaviour
         }
 
 
-        while (keepMoving) {
+       
         //Mover el personaje la derecha
         transform.Translate(new Vector3(-0.5f, 0.0f));
-        }
+     
     }
 
 
@@ -141,10 +141,10 @@ public class Enemigo : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Personaje")
+      /*  if (collision.gameObject.tag == "Personaje")
         {
             keepMoving = false; 
-        }
+        }*/
     }
 
 
